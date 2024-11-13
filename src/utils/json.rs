6c4e4a3d76ac2e30dffcdf8d6081fb8fda7ac749
sunsjay <sunsjay0806@gmail.com>
2024-11-13 19:23:57 +0800

    feat: :sparkles: fix json parse

--- src/utils/json.rs
+++ src/utils/json.rs
@@ -3,11 +3,14 @@ use serde_json::Value;
 use crate::model::VmxqStatus;
 
 pub fn parse_json(content: &str) -> Option<VmxqStatus> {
-    if let Ok(json_data) = serde_json::from_str::<VmxqStatus>(content) {
-        info!("解析JSON数据成功: {}", json_data);
-        Some(json_data)
-    } else {
-        error!("解析JSON数据失败");
-        None
+    match serde_json::from_str::<VmxqStatus>(content) {
+        Ok(json_data) => {
+            info!("解析JSON数据成功: {}", json_data);
+            Some(json_data)
+        },
+        Err(err) => {
+            error!("解析JSON数据失败: {}", err);
+            None
+        }
     }
 }
