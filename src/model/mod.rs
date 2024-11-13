d1849cc07e268eccd4e39fb7ecaecb588d473aba
sunsjay <sunsjay0806@gmail.com>
2024-11-13 19:57:51 +0800

    feat: :sparkles: fix

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -5,6 +5,7 @@ use serde::{Deserialize, Serialize};
 
 
 #[derive(Debug, Deserialize,Serialize)]
+#[serde(rename_all = "camelCase")]
 pub struct VmxqStatus {
     has_deleted: Option<bool>,
     has_id: Option<bool>,
