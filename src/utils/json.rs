60e3495f2edb72d2c06671b7d3a39c40bdfe1128
sunsjay <sunsjay0806@gmail.com>
2024-11-21 20:40:46 +0800

    refactor: :recycle: info

--- src/utils/json.rs
+++ src/utils/json.rs
@@ -1,11 +1,18 @@
-use log::{error, info};
+/*
+ * @Date: 2024-11-13 16:47:06
+ * @LastEditors: sunsjay sunsjay0806@gmail.com
+ * @LastEditTime: 2024-11-21 20:39:39
+ * @FilePath: /vm-control/src/utils/json.rs
+ * @Description: 
+ */
+use log::error;
 
 use crate::model::VmxqStatusVar;
 
 pub fn parse_json(content: &str) -> Option<VmxqStatusVar> {
     match serde_json::from_str::<VmxqStatusVar>(content) {
         Ok(json_data) => {
-            info!("解析JSON数据成功: {:?}", json_data);
+            // info!("解析JSON数据成功: {:?}", json_data);
             Some(json_data)
         },
         Err(err) => {
